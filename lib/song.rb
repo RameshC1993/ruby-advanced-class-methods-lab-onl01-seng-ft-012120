bdead59fff0c9df933afeb21dcf34536ea2bafbc
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-02-03 18:41:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,5 +37,13 @@ class Song
     end
     return nil
   end
+  
+  def self.find_or_create_by_name(name)
+    if find_by_name(name)
+      return find_by_name(name)
+    else
+      create_by_name(name)
+    end
+  end
 end
 
